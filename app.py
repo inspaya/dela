5dfc572cd66421b0455ab212f3e945e2e599276c
Muizudeen Kusimo <muizz@inspaya.com>
2022-03-26 14:14:34 +0100

    Initialize and apply rate-limiter to routes with default/custom values

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+from os import environ
 from secrets import token_hex
-from flask import Flask, request
-from markupsafe import escape
-from expiringdict import ExpiringDict
 
-CACHE_MAX_LEN = 1000
-CACHE_MAX_AGE_SECS = 604800
+from expiringdict import ExpiringDict
+from flask import Flask, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from markupsafe import escape
+
+CACHE_MAX_LEN = environ.get('CACHE_MAX_LEN') or 1000
+CACHE_MAX_AGE_SECS = environ.get('CACHE_MAX_AGE_SECS') or 604800
+DEFAULT_RATE_LIMIT = environ.get('DEFAULT_RATE_LIMIT') or "3/minute"  # we use this as a minimum value
 
 app = Flask(__name__)
+limiter = Limiter(
+    app=app,
+    key_func=get_remote_address,
+    default_limits=[DEFAULT_RATE_LIMIT]
+)
 
 
 def _create_cache(cache_max_len=CACHE_MAX_LEN, cache_max_age=CACHE_MAX_AGE_SECS):
@@ -28,7 +38,13 @@ def _get_message_by_id(message_id):
 cache = _create_cache()
 
 
+@app.errorhandler(429)
+def rate_limit_exceeded(e):
+    return f"Sorry, you have exceeded your {e.description} rate limit.", 429
+
+
 @app.route("/messages/new", methods=['POST'])
+@limiter.limit("5/minute", override_defaults=True)
 def create_message():
     request_data = request.get_json()
     if 'message' in request_data.keys():
